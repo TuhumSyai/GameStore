93cc132b9f1eda2d050805ff4fcd4f335d1ad5b0
Arman Sabit <sabitarman@gmail.com>
2025-04-16 15:52:53 +0500

    Create DB and more scheme

--- GameStore/GameStore/settings.py
+++ GameStore/GameStore/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'GameStore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'GameStore',
+        'USER': 'Admin',
+        'PASSWORD': '12345678',
+        'HOST': 'localhost',  # или IP-адрес твоего PostgreSQL сервера
+        'PORT': '5432',  # стандартный порт PostgreSQL
     }
 }
 
