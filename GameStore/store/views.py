935241d082fe698e3f75bbc3800a44670b9149b8
TuhumSyai <gog.kobyakov@gmail.com>
2025-04-20 00:43:30 +0500

    add paginator

--- GameStore/store/views.py
+++ GameStore/store/views.py
@@ -7,6 +7,7 @@ from .forms import RegisterForm, LoginForm
 from .models import Game, Genre
 import random
 from django.db.models import Q
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 User = get_user_model()
 
@@ -14,10 +15,11 @@ User = get_user_model()
 def index(request):
     random_games = Game.objects.filter(background_image__isnull=False).order_by('?')[:7]
 
-    # Делаем баннер на основе первой игры (или какой-то другой логики)
+    if not random_games.exists():
+        messages.warning(request, 'Нет доступных игр для отображения.')
+
     banner_game = random_games[0] if random_games else None
 
-    # Сериализуем нужные данные
     games_data = [
         {
             'name': game.name,
@@ -30,8 +32,8 @@ def index(request):
     context = {
         'title': 'GameStore - Главная',
         'random_games': random_games,
-        'games_data': games_data,  # Добавляем сериализованные данные
-        'banner_game': banner_game,  # Передаем конкретную игру для баннера
+        'games_data': games_data,
+        'banner_game': banner_game,
     }
 
     return render(request, 'store/index.html', context)
@@ -83,25 +85,40 @@ def game_api(request):
 def gamelist(request):
     sort_option = request.GET.get('sort', 'added')
     selected_genres = request.GET.getlist('genres')
+    page = request.GET.get('page', 1)
 
     games = Game.objects.all()
 
     if selected_genres:
         games = games.filter(genres__id__in=selected_genres).distinct()
 
+    if not games.exists():
+        messages.warning(request, 'В базе данных нет игр по выбранным фильтрам.')
+
     if sort_option == 'added':
-        games = games.order_by('-id')  # предположим, новые сверху
+        games = games.order_by('-id')
     elif sort_option == '-released':
         games = games.order_by('-released')
     elif sort_option == '-metacritic':
         games = games.order_by('-rating')
 
+    # Пагинация: 12 игр на страницу
+    paginator = Paginator(games, 15)
+    try:
+        games_page = paginator.page(page)
+    except PageNotAnInteger:
+        games_page = paginator.page(1)
+    except EmptyPage:
+        games_page = paginator.page(paginator.num_pages)
+
     context = {
         'title': 'GameStore - Игры',
-        'games': games,
+        'games': games_page,  # это page-объект
         'genres': Genre.objects.all(),
-        'selected_genres': list(map(int, selected_genres)),  # для чекбоксов
+        'selected_genres': list(map(int, selected_genres)),
         'sort_option': sort_option,
+        'paginator': paginator,
+        'page_obj': games_page,
     }
 
     return render(request, 'store/games-list.html', context)
