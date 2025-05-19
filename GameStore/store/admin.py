1eee6ecd4557f5bed8d83ab0ae8793e9ce1c6b0d
TuhumSyai <gog.kobyakov@gmail.com>
2025-05-19 17:56:48 +0500

    fix admin.py

--- GameStore/store/admin.py
+++ GameStore/store/admin.py
@@ -2,13 +2,38 @@ from django.contrib import admin
 from .models import Game, Platform, Genre, Store, CustomUser, Comment, Purchase
 from django.db.models import Count, Sum
 from django.utils.html import format_html
-
-admin.site.register(Genre)
-admin.site.register(Platform)
-admin.site.register(Store)
-admin.site.register(Comment)
-
 from django.contrib.auth.admin import UserAdmin
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Platform)
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'domain')
+    search_fields = ('name',)
+    list_filter = ('name',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'game', 'short_content', 'created_at')
+    search_fields = ('user__username', 'game__name', 'content')
+    list_filter = ('created_at', 'game', 'user')
+
+    def short_content(self, obj):
+        return obj.content[:50] + ('...' if len(obj.content) > 50 else '')
+    short_content.short_description = 'Содержание'
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
@@ -18,9 +43,9 @@ class CustomUserAdmin(UserAdmin):
     ordering = ['email']
 
     fieldsets = (
-        (None, {'fields': ('email', 'username', 'password', 'birthdate')}),
+        (None, {'fields': ('email', 'username', 'password', 'birthdate', 'bio', 'avatar')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Dates', {'fields': ('last_login',)}),
+        ('Dates', {'fields': ('last_login', 'date_joined', 'last_activity')}),
     )
 
     add_fieldsets = (
@@ -30,19 +55,26 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
 
+
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
     list_display = ('name', 'released', 'rating', 'price_kzt', 'total_purchases')
+    search_fields = ('name',)
+    list_filter = ('genres', 'platforms', 'stores', 'released')
 
     def total_purchases(self, obj):
         return obj.purchases.count()
     total_purchases.short_description = "Кол-во покупок"
 
+
 @admin.register(Purchase)
 class PurchaseAdmin(admin.ModelAdmin):
     list_display = ('user', 'game', 'purchase_date', 'price_kzt')
+    list_filter = ('purchase_date', 'game', 'user')
+    search_fields = ('user__username', 'game__name')
 
     def total_revenue(self, request, queryset):
         total = queryset.aggregate(Sum('price_kzt'))['price_kzt__sum'] or 0
         self.message_user(request, f'Общая выручка: {total} ₸')
+
     actions = ['total_revenue']
